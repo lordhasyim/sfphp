bbc3aa1cf6324545d22f8edfb29119b08ba89343
Hasyim <lordhasyim@gmail.com>
2017-02-02 19:00:58 +0700

    adding show error script

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php 
 
+// utk menampilkan pesan error atau warning
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+error_reporting(-1);
+
+//utk debug, jika error maka array tidak kosong/ada isi errornya
+/*register_shutdown_function(function() {
+print_r(error_get_last());
+});*/
+
 require_once __DIR__ . "/autoloader.php";
 
 use lib\mvc\router;
